a1b2f58727afdb1999495e804977dc1c29df313c
Sushmita Gupta <148859210+SushmitaaGupta@users.noreply.github.com>
2023-12-21 11:03:03 +0530

    Commit Changes

---
+++ customItems.java
@@ -0,0 +1,17 @@
+package com.example.wallpaperhd;
+
+public class customItems {
+    String url;
+
+    public customItems(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
